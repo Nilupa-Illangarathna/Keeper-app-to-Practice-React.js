b81cf646b6942e893a13c1d1f2678cbf61b348d9
Nilupa Illangarathna <nilupalakshantha50@gmail.com>
2023-03-24 18:42:30 +0530

    First commit

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+let date = new Date().getFullYear();
+
+
+function Footer(){
+    return <footer>
+        <p>Copyright owned by Black_Scorpion_V - {date}</p>
+    </footer>
+}
+
+export default Footer;
